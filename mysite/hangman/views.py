261fc46f0a4e71fcb80a3a4836b280e0709d1e75
daleymike <98833177+daleymike@users.noreply.github.com>
2022-10-06 09:34:43 -0400

    index view added, html page template added

--- mysite/hangman/views.py
+++ mysite/hangman/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Word
 
 def index(request):
-    return HttpResponse("Hello World")
+    hangman_words = Word.objects.all()
+    context = {
+        'hangman_words' : hangman_words
+    }
+    return render(request, 'hangman/index.html', context)
+
