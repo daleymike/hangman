baebaf8a3250b0219a2a5854693fd5af89cbcb2c
daleymike <98833177+daleymike@users.noreply.github.com>
2022-10-15 10:30:51 -0400

    random word created, implemented using underscores

--- mysite/hangman/views.py
+++ mysite/hangman/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Word
+import random
 
 def index(request):
     hangman_words = Word.objects.all()
+    random_word = random.choice(hangman_words)
     context = {
-        'hangman_words' : hangman_words
+        'hangman_words' : hangman_words,
+        'random_word' : random_word,
+        'random_word_array': list(random_word.word_text),
     }
     return render(request, 'hangman/index.html', context)
 
